fa049e5e8a9d108ff1a6779e994c285891a4d636
Roshita Dande <roshitadande@gmail.com>
2024-07-29 10:51:12 +0530

    third commit

--- app/Http/Controllers/CountryApiController.php
+++ app/Http/Controllers/CountryApiController.php
@@ -10,4 +10,28 @@ class CountryApiController extends Controller
     public function showCountry(){
        return Country::all();
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+        ]);
+
+        $countries = Country::create($request->all());
+        return response()->json($countries, 201);
+    }
+    public function update(Request $request, Country $countries)
+    {
+        $request->validate([
+            'name'=>'required',
+        ]);
+
+        $countries->update($request->all());
+        return response()->json($countries, 200);
+    }
+    public function destroy(Country $countries)
+    {
+        $countries->delete();
+        return response()->json(null, 204);
+    }
 }
